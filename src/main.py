145c6a6dc5f9935ff5a4ba2ae6afc7388c92c95e
MAIKOL\HP <mgarrido2503@gmail.com>
2023-06-13 08:43:19 +0200

    se modifico la main.py, y se paso el codigo  a programacion orientada a objeto

--- src/main.py
+++ src/main.py
@@ -1,35 +1,13 @@
-import tkinter as tk
+from taximetro import Taximetro
 
+def main():
+    taxi = Taximetro()
+    taxi.correr()
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
 
-
-
-tarifaEnMovimiento=5
-tarifaSinMovimiento=3
-contador = 0
-total = 0
-
-
-def arrancar():
- return
-
-
-
-def frenar():
- return
-
-
-
-def terminarCarrera():
- return
- 
- 
- 
-
-window = tk.Tk()
-window.title("Programa de Saludos")
-window.geometry("1080x600")
-submit_button = tk.Button(window, text="Arrancar", command=arrancar)
-submit_button = tk.Button(window, text="Frenar", command=frenar)
-submit_button = tk.Button(window, text="Terminar Carrera", command=terminarCarrera)
-submit_button.pack() #
-window.mainloop()
